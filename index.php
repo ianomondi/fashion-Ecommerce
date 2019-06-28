9e2d43a19b2b933f0743030ec01fbbc6cf68bc4e
ianomondi <ianochieng14@gmail.com>
2019-06-28 06:44:44 +0300

    Update index.php
    Home page redesign

--- index.php
+++ index.php
@@ -95,60 +95,7 @@
        <div class="container"><!-- container Begin -->
            
            <div class="same-height-row"><!-- same-height-row Begin -->
-               
-               <div class="col-sm-4"><!-- col-sm-4 Begin -->
-                   
-                   <div class="box same-height"><!-- box same-height Begin -->
-                       
-                       <div class="icon"><!-- icon Begin -->
-                           
-                           <i class="fa fa-heart"></i>
-                           
-                       </div><!-- icon Finish -->
-                       
-                       <h3><a href="#">Best Offer</a></h3>
-                       
-                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-                       
-                   </div><!-- box same-height Finish -->
-                   
-               </div><!-- col-sm-4 Finish -->
-               
-               <div class="col-sm-4"><!-- col-sm-4 Begin -->
-                   
-                   <div class="box same-height"><!-- box same-height Begin -->
-                       
-                       <div class="icon"><!-- icon Begin -->
-                           
-                           <i class="fa fa-tag"></i>
-                           
-                       </div><!-- icon Finish -->
-                       
-                       <h3><a href="#">Best Prices</a></h3>
-                       
-                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                       
-                   </div><!-- box same-height Finish -->
-                   
-               </div><!-- col-sm-4 Finish -->
-               
-               <div class="col-sm-4"><!-- col-sm-4 Begin -->
-                   
-                   <div class="box same-height"><!-- box same-height Begin -->
-                       
-                       <div class="icon"><!-- icon Begin -->
-                           
-                           <i class="fa fa-thumbs-up"></i>
-                           
-                       </div><!-- icon Finish -->
-                       
-                       <h3><a href="#">100% Original</a></h3>
-                       
-                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                       
-                   </div><!-- box same-height Finish -->
-                   
-               </div><!-- col-sm-4 Finish -->
+             
                
            </div><!-- same-height-row Finish -->
            
